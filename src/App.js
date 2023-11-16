62a0eda5042c059c067e616b30785776aa0c41e0
young <rhsiddlsidhd@nate.com>
2023-11-17 01:30:30 +0900

    :art: Design:dropdown 구현중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "../src/components/common/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
