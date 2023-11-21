c2405a433bd3a155c1126282cf50ac03e1b5b832
young <rhsiddlsidhd@nate.com>
2023-11-21 11:36:52 +0900

    :bug: Fix:fontawesome모듈 설치 및 htmlFor 변경

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 import ProductDetailSection from "../components/detail/ProductDetailSection";
 const Detail = () => {
+  const randomId = () => {
+    return Math.random().toString(36).substr(2, 16);
+  };
+
+  const product = {
+    id: randomId(),
+  };
+  console.log(product.id);
+
   return (
     <ProductDetail>
       <ProductDetailSection />
