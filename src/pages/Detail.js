d3eafa4ca0cbce57e4828facd4cd9a5c2cae6134
young <rhsiddlsidhd@nate.com>
2023-11-22 07:43:09 +0900

    :zap: Feat:ImgSlide
    * Related to #7

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,18 +1,35 @@
 import styled from "styled-components";
 import ProductDetailSection from "../components/detail/ProductDetailSection";
+import { useState, useEffect } from "react";
 const Detail = () => {
+  const [data, setData] = useState([]);
+
+  //목데이터
+
   const randomId = () => {
     return Math.random().toString(36).substr(2, 16);
   };
 
   const product = {
-    id: randomId(),
+    Id: randomId(),
+    Name: "베어 자수 케일리 스커프 털 슬리퍼",
+    Code: "RFS11478W",
+    Price: 129000,
+    Size: [220, 225, 230, 235, 240, 245, 250],
+    Imgs: [
+      process.env.PUBLIC_URL + `/Images/shoes1.jpg`,
+      process.env.PUBLIC_URL + `/Images/shoes2.jpg`,
+      process.env.PUBLIC_URL + `/Images/shoes3.jpg`,
+    ],
   };
-  console.log(product.id);
+
+  useEffect(() => {
+    setData(product);
+  }, []);
 
   return (
     <ProductDetail>
-      <ProductDetailSection />
+      <ProductDetailSection data={data} />
     </ProductDetail>
   );
 };
